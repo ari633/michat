58f98ef4f51ab93db581fdd63296b8debe28b362
Ari Pratomo <ari.pratomo@ajaib.co.id>
2023-11-26 13:30:41 +0700

    client build: setup client build

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,3 +1,4 @@
 import { io } from "socket.io-client";
+const SOCKET_WS = process.env.REACT_APP_SOCKET_WS || "ws://localhost:9090";;
 
-export const socket = io("ws://localhost:9090", { autoConnect: false });
+export const socket = io(SOCKET_WS, { autoConnect: false });
